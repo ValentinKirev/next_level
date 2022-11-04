159d83d6a5da426bf9d7dddaac1e7de5406954ec
Valentin Kirev <valentinkirev666@gmail.com>
2022-11-04 14:17:00 +0200

    Created games and news apps

--- next_level/settings.py
+++ next_level/settings.py
@@ -15,6 +15,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'next_level.news',
+    'next_level.games'
 ]
 
 MIDDLEWARE = [
