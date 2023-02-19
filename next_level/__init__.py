f4d9f90036826096fc1e0f3cbf52c57daa920ace
Valentin Kirev <valentinkirev666@gmail.com>
2023-02-19 21:29:12 +0200

    Last changes

--- next_level/__init__.py
+++
@@ -1,3 +0,0 @@
-from next_level.celery import celery_app
-
-__all__ = ('celery_app', )
--- next_level/accounts/signals.py
+++ next_level/accounts/signals.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
+from next_level import settings
 from next_level.accounts.models import Profile
-from next_level.accounts.tasks import send_greeting_email
 
 UserModel = get_user_model()
 
@@ -18,7 +19,10 @@ def create_profile(sender, instance, created, **kwargs):
         plain_message = strip_tags(html_message)
         to = instance.email
 
-        send_greeting_email.delay(subject, plain_message, to, html_message)
+        send_mail(subject=subject,
+                  message=plain_message,
+                  from_email=settings.DEFAULT_FROM_EMAIL,
+                  recipient_list=[to],
+                  html_message=html_message)
 
         Profile.objects.create(user=instance)
-
